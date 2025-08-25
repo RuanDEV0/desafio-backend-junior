ab8b3d0c9be3c6f379ae9d9ccb05dea063e0a9e8
Ruan <oliveiraruan2018@gmail.com>
2025-08-25 15:14:26 -0400

    feat: essentials methods structure

--- challenge/src/ActiveService.java
+++ challenge/src/ActiveService.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActiveService {
+    private static final List<Active> activeList = new ArrayList<>();
+
+    public static void save(Active active){
+        activeList.add(active);
+    }
+
+    public static List<Active> getActiveList(){
+        return activeList;
+    }
+
+    public static void reportSale(String code, int quantity){
+
+    }
+
+    public static List<Object> summary(){
+        return new ArrayList<>();
+    }
 }
